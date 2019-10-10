52f842db7b0c7e597160a8692e426ae3d1da8404
Prashant Chhabra <prashant.chhabra89@gmail.com>
2019-10-10 14:38:03 -0700

    make timeout work properly

--- test/smoke/tests/requestUrls.js
+++ test/smoke/tests/requestUrls.js
@@ -4,15 +4,10 @@ let urls = require('./urls.json');
 const contentChecker = require('./../../helpers/contentChecker');
 const assert = require('assert');
 
-const concurrency = 1; // Don't wanna blow their server. So, one request at a time.
 const delayms = 2000; // Just adding some delay to not blow their server
 
-const delay = Promise.coroutine(function*() {
-  yield Promise.delay(delayms);
-});
-urls = [urls[0]];
 describe('url fetch results', () => {
-  urls.forEach((urlObj) => {
+  urls.forEach(async (urlObj) => {
     describe('Test different site urls', () => {
       it('Test ' + urlObj.url, async () => {
         console.time(`sendRequest${urlObj.url}`);
@@ -20,12 +15,13 @@ describe('url fetch results', () => {
           uri: urlObj.url,
           simple: false,
         });
-        console.timeEnd(`sendRequest${urlObj.url}`);
         const correctContent = contentChecker.checkContentFromURL(
           urlObj.url,
           result,
         );
+        console.timeEnd(`sendRequest${urlObj.url}`);
         assert.equal(correctContent, true);
+        await Promise.delay(delayms);
       });
     });
   });
