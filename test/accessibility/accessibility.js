724e665add7aae7beac91bd8b826d5b02150cba2
Prashant Chhabra <prashant.chhabra89@gmail.com>
2019-10-11 10:01:56 -0700

    update lint

--- test/accessibility/accessibility.js
+++ test/accessibility/accessibility.js
@@ -1,6 +1,8 @@
 const pa11y = require('pa11y');
- 
-pa11y('https://www.covenanthousebc.org'/*, {standard: 'WCAG2AAA'}*/).then((results) => {
+
+pa11y('https://www.covenanthousebc.org' /*, {standard: 'WCAG2AAA'}*/).then(
+  (results) => {
     // Do something with the results
     console.log(results);
-});
+  },
+);
